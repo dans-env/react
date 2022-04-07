39a179ed2ede0eb9fef884d9b2bb60b02a6d2ca1
dans-env <blusplatt@gmail.com>
2022-04-07 15:27:05 +0200

    Create react app - code clean up. Removed avything than was not needed

--- ecommerce-app/src/index.js
+++ ecommerce-app/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+
+//css import
+import './assets/css/index.css';
+
+//component imports
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const mountPoint = document.getElementById('app-mount');
+const app = createRoot(mountPoint);
+app.render (
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
