9f181aa33bf111eab52873c72afdef8700f48ff3
dans-env <blusplatt@gmail.com>
2022-04-21 08:28:27 +0200

    Adding todo checked and unchecked functionality

--- todo-app/src/components/Header.js
+++ todo-app/src/components/Header.js
@@ -1,12 +1,16 @@
+import { useContext, useState } from "react";
+import { TodoContext } from "../contexts/TodoContext";
 import Logo from "./Logo";
 import Button from "./Button";
 
 function Header() {
+   const { todos, setTodos } = useContext(TodoContext);
+
    return(
       <header className="header">
          <div className="container">
             <Logo />
-            <p className="stats">Some text with stats about the interface</p>
+            <p className="stats">{`Total items: ${todos.length} - Items complete: 0`}</p>
          </div>
          <div className="container">
             <Button buttonClass="add-todo" buttonText="add" />
--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -9,9 +9,27 @@ function Todo(props) {
       setTodos(prevTotos => prevTotos.filter(todo => todo.props.name !== name));
    };
 
+   const markTodoAsComplete = (todo) => {
+      todo.classList.add("checked");
+      todo.style.webkitFilter = "grayscale(60%)";
+   };
+
+   const unmarkTodoAsComplate = (todo) => {
+      todo.classList.remove("checked");
+      todo.style.webkitFilter = "grayscale(0%)";
+   }
+
+   const checkTodoState = (event) => {
+      const todo = event.target.parentNode.parentNode;
+      (!todo.classList.contains("checked")) ? markTodoAsComplete(todo) : unmarkTodoAsComplate(todo);
+   }
+
    return(
       <div className="todo-item" name={props.name}>
-         <p contentEditable={true} suppressContentEditableWarning={true}>Edit me!</p>
+         <div className="content">
+            <div className="checkbox" onClick={checkTodoState}></div>
+            <p contentEditable={true} suppressContentEditableWarning={true}>Edit me!</p>
+         </div>
          <span className="bin" onClick={deleteTodo}></span>
       </div>
    );
