55cc23e306185c66eebf1146df06e509df54b35f
dans-env <blusplatt@gmail.com>
2022-04-21 07:49:35 +0200

    Clean up todo JSX

--- todo-app/src/components/Todo.js
+++ todo-app/src/components/Todo.js
@@ -5,17 +5,14 @@ function Todo(props) {
    const { todos, setTodos } = useContext(TodoContext);
    
    const deleteTodo = (event) => {
-      const name = event.target.parentNode.parentNode.getAttribute("name");
+      const name = event.target.parentNode.getAttribute("name");
       setTodos(prevTotos => prevTotos.filter(todo => todo.props.name !== name));
    };
 
    return(
       <div className="todo-item" name={props.name}>
          <p contentEditable={true} suppressContentEditableWarning={true}>Edit me!</p>
-         <div className="icons-container">
-            <span className="edit"></span>
-            <span className="bin" onClick={deleteTodo}></span>
-         </div>
+         <span className="bin" onClick={deleteTodo}></span>
       </div>
    );
 };
