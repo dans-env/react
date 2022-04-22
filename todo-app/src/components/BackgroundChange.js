f3a24d003ec22935ae1c0d8b9728eb65c0e0a212
dans-env <blusplatt@gmail.com>
2022-04-22 08:44:00 +0200

    Adding background change functionality

--- todo-app/src/components/BackgroundChange.js
+++ todo-app/src/components/BackgroundChange.js
@@ -1,13 +1,37 @@
 function BackgroundChange() {
+
+   const changeBackground = (event) => {
+      const backgroundElement = document.querySelector(".body");
+      const elementClass = event.target.classList.value;
+      
+      if(elementClass === "abstract") {
+        backgroundElement.classList.remove(backgroundElement.classList[1]);
+        backgroundElement.classList.add("abstract");
+        return;
+      }
+
+      if(elementClass === "buildings") {
+         backgroundElement.classList.remove(backgroundElement.classList[1]);
+         backgroundElement.classList.add("buildings");
+         return;
+      }
+
+      if(elementClass === "mountain") {
+         backgroundElement.classList.remove(backgroundElement.classList[1]);
+         backgroundElement.classList.add("mountain");
+         return;
+      }
+   };
+
    return(
       <>
          <div className="change-background">
             <span className="background-icon"></span>
             <div className="background-list">
                <ul>
-                  <li className="abstract">Abstract</li>
-                  <li className="buildings">Building</li>
-                  <li className="mountain">Mountain</li>
+                  <li className="abstract" onClick={changeBackground}>Abstract</li>
+                  <li className="buildings" onClick={changeBackground}>Building</li>
+                  <li className="mountain" onClick={changeBackground}>Mountain</li>
                </ul>
             </div>
          </div>
