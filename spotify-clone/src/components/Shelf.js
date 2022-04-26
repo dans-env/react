214b3251bd3cfb8b2b709b5f01c74f6617a81d19
dans-env <blusplatt@gmail.com>
2022-04-26 08:30:38 +0200

    Adding all new build changes

---
+++ spotify-clone/src/components/Shelf.js
@@ -0,0 +1,15 @@
+const Shelf = () => {
+   return(
+      <section className="shelf">
+         <div className="shelf-title">
+            <h3>Title</h3>
+            <a href="#">See all</a>
+         </div>
+         <div className="shelf-content">
+
+         </div>
+      </section>
+   );
+};
+
+export default Shelf;
--- spotify-clone/src/templates/MainTemplate.js
+++ spotify-clone/src/templates/MainTemplate.js
@@ -1,5 +1,7 @@
+//used components
 import Navigation from "../components/Navigation";
 import TopBar from "../components/TopBar";
+import Shelf from "../components/Shelf";
 
 const MainTemplate = () => {
    return (
@@ -10,6 +12,9 @@ const MainTemplate = () => {
          <div className="main-view">
             <div className="gradient-background"></div>
             <TopBar />
+            <section className="main-content-view">
+               <Shelf />
+            </section>
          </div>
       </div>
    );
